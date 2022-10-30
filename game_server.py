6f6cc8c65551be6f8925421f6fb2997e91513864
송민섭 <minsub1031@gmail.com>
2022-10-30 22:08:38 +0900

    Update game_server.py

--- game_server.py
+++ game_server.py
@@ -78,7 +78,7 @@ def handle_receive(client_socket, user, i):
             #유저 목록에서 방금 종료한 유저의 정보를 삭제
             del rooms[str(i)][user]
             print(rooms[str(i)].keys())
-            string = "destroy : %s" % user
+            string = "destroy:%s" % user
             msg_func(string, i)
 
             break
